4ff860dbb53d40a96dd5a1c9c32ba47e6986eedb
wsb347 <wsb347@naver.com>
2021-05-14 01:36:54 +0900

    관리자 홈 살짝 수정(미완)

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,8 +1,12 @@
 package com.sbs.untact.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UsrHomeController {
-	
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "usr/home/main";
+	}
 }
